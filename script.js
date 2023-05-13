f3b9c0ace70b2d8abeefacd54d2662ebb0e2ba74
Livara <livara@gmail.com>
2023-05-13 12:36:31 +0200

    Create functions to make it work like calculate()

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
-const calculator = document.getElementById("calculator");
-const buttons = ["1", "2", "3", "+", "4", "5", "6", "-", "7", "8", "9", "*", "0", "C", "=", "/"];
 
-for (let i = 0; i < buttons.length; i++) {
-    const button = document.createElement("button");
+var displayField = document.getElementById("result");
+var calculator = document.getElementById("calculator");
+var buttons = ["1", "2", "3", "+", "4", "5", "6", "-", "7", "8", "9", "*", "0", "C", "=", "/"];
+
+for (var i = 0; i < buttons.length; i++) {
+    var button = document.createElement("button");
     button.textContent = buttons[i];
 
+    if (buttons[i] === "C") {
+        button.classList.add("clear");
+        button.addEventListener("click", clearResult);
+    } else if (buttons[i] === "=") {
+        button.classList.add("equal");
+        button.addEventListener("click", calculateResult);
+    } else if (isNaN(parseInt(buttons[i]))) {
+        button.classList.add("operator");
+        button.addEventListener("click", function() {
+            display(this.textContent);
+        });
+    } else {
+        button.addEventListener("click", function() {
+            display(this.textContent);
+        });
+    }
 
+    calculator.appendChild(button);
+}
+
+function display(value) {
+    displayField.value += value;
+}
+
+function clearResult() {
+    displayField.value = "";
+}
+
+function calculateResult() {
+    const result = eval(displayField.value);
+    displayField.value = result;
 }
